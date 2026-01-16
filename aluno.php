1c462fa9a9726dff47a610406922abd0255f22e7
jonas <jonassouza.information@gmail.com>
2026-01-16 10:56:48 -0300

    alterei o <title> no arquivo principal

--- aluno.php
+++ aluno.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dfdgdg</title>
+    <title>desempenho-mensal</title>
 </head>
 <body>
     <h2>Sistema-de-controle-de-desempenho-mensal</h2>
