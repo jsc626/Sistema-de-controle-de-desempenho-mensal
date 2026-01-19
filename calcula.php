cfb81e2b7902ef7acf6d2350414b48b83f5bf0ce
jonas <jonassouza.information@gmail.com>
2026-01-19 18:33:37 -0300

    atualizando o commit

--- calcula.php
+++ calcula.php
@@ -1,18 +1,26 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-   $nome = (int)$_POST["nome"];
-   $tarefas = $_POST["tarefas"];
-   $atraso = $_POST["atraso"];
+   $nome = $_POST["nome"];
+   $tarefas = (int)$_POST["tarefas"];
+   $atraso = (int)$_POST["atraso"];
 
 if(!empty($nome)){
-    echo "Funcionario:$nome";
+    echo "Funcionario:$nome<br>";
 }
 
-if($tarefas >= 20 && $atraso <= 1){
+if($tarefas >=25 && $atraso == 0){
+    echo "Funcionario elegivel para bonus";
+}
+
+elseif($tarefas == 20 && $atraso <= 1){
     echo "Desempenho Excelente";
-}elseif($tarefas >= 10 && $tarefas <=19 && $atraso <=3){
+}
+
+elseif($tarefas >= 10 && $tarefas <=19 && $atraso <=3){
     echo "Desempenho Regular";
-}else{
+}
+
+else{
     echo "desempenho Insatisfatório";
 }
 
