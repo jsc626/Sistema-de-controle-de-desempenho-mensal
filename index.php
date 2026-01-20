4274d3f416d86d07c452de176f4efb71aab7c530
jonas <jonassouza.information@gmail.com>
2026-01-19 21:09:16 -0300

    atualizando o index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>desempenho-mensal</title>
+</head>
+<body>
+    <h2>Sistema-de-controle-de-desempenho-mensal</h2>
+    <form action="calcula.php" method="POST">
+
+    <div>
+        <input type="text" name="setor" placeholder="Qual é o seu setor">
+    </div>
+     
+    <div>
+    <input type="text" name="nome" placeholder="Digite seu nome">
+    </div>
+
+    <div>
+    <input type="text" name="tarefas" placeholder="Quantas tarefas vc fez">
+    </div>
+
+    <div>
+    <input type="text" name="atraso" placeholder="Quantos atrasos vc tem ">
+    </div>
+    
+        <input type="submit" value="Consultar desempenho">
+    
+    </form>
+</body>
+</html>
